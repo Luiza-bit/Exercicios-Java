2c1891d02d6bccdf1b2b76dd82904a6d2a28a6f1
Luiza Rosa <luizarosademedeiros18@outlook.com>
2026-04-18 16:49:45 -0400

    git commit

---
+++ Exercicios/src/mundo/PNZ.java
@@ -0,0 +1,18 @@
+package mundo;
+import java.util.Scanner;
+public class PNZ {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int num = sc.nextInt();
+
+        if (num > 0) {
+            System.out.println("Positivo");
+        } else if (num < 0) {
+            System.out.println("Negativo");
+        } else {
+            System.out.println("O número é Zero");
+        }
+        sc.close();
+    }
+}
